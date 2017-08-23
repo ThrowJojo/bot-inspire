9425aa6224c724b88f4679911f98c1a79d3eb384
Jordan Holland <thisrememo@gmail.com>
2017-08-23 15:59:16 +1000

    Added quick reply functionality.

---
+++ src/bot/BotHelper.js
@@ -0,0 +1,51 @@
+// @flow
+
+import FBBotFramework from 'fb-bot-framework';
+import Strings from '../language/strings.json';
+import Constants from '../Constants';
+import Functions from '../Functions';
+
+export default class BotHelper {
+
+    botInstance: FBBotFramework;
+
+    constructor(botInstance: FBBotFramework) {
+        this.botInstance = botInstance;
+        this.setup();
+    }
+
+    setup() {
+        this.botInstance.setGreetingText(Strings.greeting);
+        this.botInstance.setGetStartedButton(Constants.PAYLOAD_GET_STARTED);
+        // TODO: Add persistent menu options
+    }
+
+    // Sends standard quick replies with specified message
+    sendQuickReplies(userId: string, message: string) {
+        let quickReplies = [
+            {
+                content_type: 'text',
+                title: Strings.inspire,
+                payload: Constants.PAYLOAD_INSPIRE
+            }
+        ];
+        this.botInstance.sendQuickReplies(userId, message, quickReplies);
+    }
+
+    // Handles the 'Get Started' button interaction
+    handleGetStarted(userId: string) {
+        this.sendQuickReplies(userId, Strings.explanation);
+    }
+
+    // Handles a message from the user
+    handleMessage(userId: string) {
+        this.sendQuickReplies(userId, Strings.help);
+    }
+
+    // Sends an inspirational message to the specified user
+    sendInspirationalMessage(userId: string) {
+        let randomQuote = Functions.getRandomQuote();
+        this.sendQuickReplies(userId, `${randomQuote.quoteText} - ${randomQuote.quoteAuthor}`);
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 // @flow
 
 import express from 'express';
+import BotHelper from './bot/BotHelper';
+import FBBotFramework from 'fb-bot-framework';
+import Config from './config.json';
+import Constants from './Constants';
+
+let botInstance: FBBotFramework = new FBBotFramework({
+    page_token: Config.page_token,
+    // TODO: Change verify token
+    verify_token: Config.verify_token
+});
+
+let botHelper: BotHelper = new BotHelper(botInstance);
+let app = express();
+
+botInstance.on('postback', (userId: string, payload: string) => {
+    if (payload == Constants.PAYLOAD_GET_STARTED) {
+        botHelper.handleGetStarted(userId);
+    }
+});
+
+botInstance.on('message', (userId: string, message: string) => {
+    botHelper.handleMessage(userId);
+});
+
+botInstance.on('quickreply', (userId: string, payload: string) => {
+    if (payload == Constants.PAYLOAD_INSPIRE) {
+        botHelper.sendInspirationalMessage(userId);
+    }
+});
+
+app.use('/webhook', botInstance.middleware());
+app.get('/', (request: any, response: any) => {
+   response.send("A bot lives here...");
+});
+app.listen(3000);
+
+
+
